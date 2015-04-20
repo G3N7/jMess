159fcc2863152ce3e166ad039c2cbd9e3653e9ae
Mitch Ferrer <mr.ferfer@gmail.com>
2015-04-20 15:26:07 -0700

    rebuilding package

--- jMess.Build/output/jMess.d.ts
+++ jMess.Build/output/jMess.d.ts
@@ -1,21 +1,18 @@
-﻿/// <reference path="../Scripts/LogR.d.ts" />
 declare module jMess {
-    class EventBarker {
-        private _eventRegistry;
+    class EventRegistry implements IEventRegistry {
+        private _events;
+        private _registry;
         private _logR;
-        constructor(eventRegistry: IEventRegistry, logR: ILogR);
-        public startBarking(): void;
-        private _isInternalEvent(args);
-    }
-}
-declare module jMess {
-    class LifeCycleEvents {
-        static AfterRegister: string;
-        static BeforeRegister: string;
-        static AfterHook: string;
-        static BeforeHook: string;
-        static AfterRaise: string;
-        static BeforeRaise: string;
+        private _timeout;
+        constructor(logR: ILogR, timeout?: (delegate: () => void, delay: number) => void);
+        getAvailableEvents(): string[];
+        hook(eventToHook: string, delegate: Function): () => void;
+        raise(eventToRaise: string, data: Object): void;
+        register(eventsToRegister: string | string[] | Object): void;
+        private _registerEventsObject(eventsObj);
+        private _registerArrayOfEvents(eventsArray);
+        private _registerSingleEvent(eventToRegister);
+        private _eventExists(eventName);
     }
 }
 declare module jMess {
@@ -24,22 +21,5 @@ declare module jMess {
         hook(eventName: string, onRaise: Function): () => void;
         raise(eventToRaise: string, data: Object): void;
         register(eventsToRegister: any): void;
-    }
-}
-declare module jMess {
-    class EventRegistry implements IEventRegistry {
-        private _events;
-        private _registry;
-        private _logR;
-        private _timeout;
-        constructor(logR: ILogR, timeout?: (delegate: () => void, delay: number) => void);
-        public getAvailableEvents(): string[];
-        public hook(eventToHook: string, delegate: Function): () => void;
-        public raise(eventToRaise: string, data: Object): void;
-        public register(eventsToRegister: any): void;
-        private _registerEventsObject(eventsObj);
-        private _registerArrayOfEvents(eventsArray);
-        private _registerSingleEvent(eventToRegister);
-        private _eventExists(eventName);
     }
 }
--- jMess.Build/output/jMess.js
+++ jMess.Build/output/jMess.js
@@ -1,56 +1,8 @@
-﻿var jMess;
-(function (jMess) {
-    var EventBarker = (function () {
-        function EventBarker(eventRegistry, logR) {
-            this._eventRegistry = eventRegistry;
-            this._logR = logR;
-        }
-        EventBarker.prototype.startBarking = function () {
-            var _this = this;
-            var logger = this._logR;
-            this._eventRegistry.hook(jMess.LifeCycleEvents.BeforeRegister, function (data) {
-                logger.trace('Register: ', data);
-            });
-            this._eventRegistry.hook(jMess.LifeCycleEvents.BeforeHook, function (data) {
-                if (!_this._isInternalEvent(data[0])) {
-                    logger.trace('Hooked: ', data);
-                }
-            });
-            this._eventRegistry.hook(jMess.LifeCycleEvents.BeforeRaise, function (data) {
-                if (!_this._isInternalEvent(data[0])) {
-                    logger.info('Raise: ', data);
-                }
-            });
-            logger.trace('woof.. woof.. event barker be a bark\'n');
-        };
-
-        EventBarker.prototype._isInternalEvent = function (args) {
-            return args == jMess.LifeCycleEvents.BeforeHook || args == jMess.LifeCycleEvents.BeforeRaise || args == jMess.LifeCycleEvents.BeforeRegister || args == jMess.LifeCycleEvents.AfterHook || args == jMess.LifeCycleEvents.AfterRaise || args == jMess.LifeCycleEvents.AfterRegister;
-        };
-        return EventBarker;
-    })();
-    jMess.EventBarker = EventBarker;
-})(jMess || (jMess = {}));
-var jMess;
-(function (jMess) {
-    var LifeCycleEvents = (function () {
-        function LifeCycleEvents() {
-        }
-        LifeCycleEvents.AfterRegister = '__er_AfterRegisterEvent__';
-        LifeCycleEvents.BeforeRegister = '__er_BeforeRegisterEvent__';
-        LifeCycleEvents.AfterHook = '__er_AfterHookEvent__';
-        LifeCycleEvents.BeforeHook = '__er_BeforeHookEvent__';
-        LifeCycleEvents.AfterRaise = '__er_AfterRaiseEvent__';
-        LifeCycleEvents.BeforeRaise = '__er_BeforeRaiseEvent__';
-        return LifeCycleEvents;
-    })();
-    jMess.LifeCycleEvents = LifeCycleEvents;
-})(jMess || (jMess = {}));
 var jMess;
 (function (jMess) {
     var EventRegistry = (function () {
         function EventRegistry(logR, timeout) {
-            this._events = _.clone(jMess.LifeCycleEvents);
+            this._events = {};
             this._registry = {};
             this._logR = logR;
             this._timeout = timeout ? timeout : setTimeout;
@@ -59,25 +11,20 @@ var jMess;
             var eventCopy = _.clone(this._events);
             return _.values(eventCopy);
         };
-
         EventRegistry.prototype.hook = function (eventToHook, delegate) {
             if (eventToHook == null)
                 throw 'You must provide an event to hook, have you define the event object yet? "...oh you have to write the code! -Scott Hanselman"';
             if (delegate == null)
                 throw 'You must provide an delegate to run when the event is raised';
             if (!this._eventExists(eventToHook))
-                throw 'The event "' + eventToHook + '" your trying to hook to does not exist, make sure you have registered the events with the EventRegistry, the available events are ' + _.map(_.values(this._events), function (x) {
-                    return '\n' + x;
-                });
-            this.raise(jMess.LifeCycleEvents.BeforeHook, arguments);
-
+                throw 'The event "' + eventToHook + '" your trying to hook to does not exist, make sure you have registered the events with the EventRegistry, the available events are ' + _.map(_.values(this._events), function (x) { return '\n' + x; });
+            this._logR.trace('Hooking: ', eventToHook);
             if (this._registry[eventToHook] == null) {
                 this._registry[eventToHook] = [delegate];
-            } else {
+            }
+            else {
                 this._registry[eventToHook].push(delegate);
             }
-            this.raise(jMess.LifeCycleEvents.AfterHook, arguments);
-
             var cancelation = (function (r, e, d) {
                 return function () {
                     var indexOfDelegate = r[e].indexOf(d);
@@ -86,7 +33,6 @@ var jMess;
             })(this._registry, eventToHook, delegate);
             return cancelation;
         };
-
         EventRegistry.prototype.raise = function (eventToRaise, data) {
             var _this = this;
             if (eventToRaise == null)
@@ -94,47 +40,36 @@ var jMess;
             if (data == null)
                 throw 'data was null, consumers of events should feel confident they will never get null data.';
             if (!this._eventExists(eventToRaise))
-                throw 'The event "' + eventToRaise + '" your trying to raise does not exist, make sure you have registered the event with the EventRegistry, the available events are ' + _.map(_.values(this._events), function (x) {
-                    return '\n' + x;
-                });
-
-            if (eventToRaise !== jMess.LifeCycleEvents.BeforeRaise && eventToRaise !== jMess.LifeCycleEvents.AfterRaise) {
-                this.raise(jMess.LifeCycleEvents.BeforeRaise, arguments);
-            }
-
+                throw 'The event "' + eventToRaise + '" your trying to raise does not exist, make sure you have registered the event with the EventRegistry, the available events are ' + _.map(_.values(this._events), function (x) { return '\n' + x; });
+            this._logR.info('Raise: ', data);
             var asyncInvokation = function (delegate) {
                 var logr = _this._logR;
                 _this._timeout.call(window, function () {
-                    try  {
+                    try {
                         delegate(data);
-                    } catch (ex) {
+                    }
+                    catch (ex) {
                         logr.error('An exception was thrown when', eventToRaise, 'was Raised with', data, ex);
                     }
                 }, 100);
             };
-
             var eventDelegates = this._registry[eventToRaise];
             _.each(eventDelegates, asyncInvokation);
-
-            if (eventToRaise !== jMess.LifeCycleEvents.BeforeRaise && eventToRaise !== jMess.LifeCycleEvents.AfterRaise) {
-                this.raise(jMess.LifeCycleEvents.AfterRaise, arguments);
-            }
         };
-
         EventRegistry.prototype.register = function (eventsToRegister) {
             if (eventsToRegister == null)
                 throw 'Your events where null, we must have something';
-            this.raise(jMess.LifeCycleEvents.BeforeRegister, arguments);
+            this._logR.trace('Register: ', eventsToRegister);
             if (eventsToRegister instanceof Array) {
                 this._registerArrayOfEvents(eventsToRegister);
-            } else if (eventsToRegister instanceof Object) {
+            }
+            else if (eventsToRegister instanceof Object) {
                 this._registerEventsObject(eventsToRegister);
-            } else {
+            }
+            else {
                 this._registerSingleEvent(eventsToRegister);
             }
-            this.raise(jMess.LifeCycleEvents.AfterRegister, arguments);
         };
-
         EventRegistry.prototype._registerEventsObject = function (eventsObj) {
             for (var key in eventsObj) {
                 if (eventsObj.hasOwnProperty(key)) {
@@ -145,7 +80,6 @@ var jMess;
                 }
             }
         };
-
         EventRegistry.prototype._registerArrayOfEvents = function (eventsArray) {
             if (eventsArray.length === 0)
                 throw 'The array of events was empty :(';
@@ -156,7 +90,6 @@ var jMess;
                 this._registerSingleEvent(eventsArray[i]);
             }
         };
-
         EventRegistry.prototype._registerSingleEvent = function (eventToRegister) {
             if (typeof eventToRegister !== 'string') {
                 this._logR.warn('The event being registered is not a string, its value is ', eventToRegister);
@@ -168,7 +101,6 @@ var jMess;
                 throw 'the event you are trying to register "' + eventToRegister + '" is already registered, either you are duplicating logic or need to be more specific in your event naming';
             this._events[eventToRegister] = eventToRegister;
         };
-
         EventRegistry.prototype._eventExists = function (eventName) {
             return _.contains(_.values(this._events), eventName);
         };
